2fd7a70eb5e7569422abcc8cb859d222fca84a06
Riyad <Riyad9876@gmail.com>
2023-07-13 16:19:46 -0400

    task 1 and 2

---
+++ script.js
@@ -0,0 +1,8 @@
+const mainEl = document.querySelector('main')
+//task 1
+mainEl.style.backgroundColor = 'var(--main-bg)';
+//task 2
+const newH1 = document.createElement('h1');
+newH1.textContent = 'SEI Rocks!'
+
+mainEl.appendChild(newH1)
