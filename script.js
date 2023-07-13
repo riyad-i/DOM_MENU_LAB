a1c73eaf706cd9a12b890bbdc9ba7fd2e7674942
Riyad <Riyad9876@gmail.com>
2023-07-13 16:48:54 -0400

    task 2.2

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ mainEl.classList.add("flex-ctr")
 
 //task 2.0
 const topMenuEl = document.getElementById('top-menu')
+
+//2.1
+topMenuEl.style.height = '100%';
+//2.2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
