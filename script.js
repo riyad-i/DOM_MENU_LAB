2e5fec2499df1a5cd0a09804c8ec2bdcc704fd59
Riyad <Riyad9876@gmail.com>
2023-07-14 14:58:55 -0400

    finished task 3, working on 3.1

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+
 const mainEl = document.querySelector('main')
 //task 1
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -19,3 +28,11 @@ topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 //2.3
 topMenuEl.classList.add('flex-around')
+
+//3.1
+for (entry of menuLinks){
+    newLink = document.createElement('a')
+    console.log(newLink);
+    newLink.href = ''
+    
+}
